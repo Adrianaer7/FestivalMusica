b882ff0d5dc99e9efbfed8eb9787d3bf3bc1c09a
Adrian Roldan <adrianaer7@hotmail.com>
2022-08-03 19:54:43 -0300

    SD

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,30 @@
 //Un pipe es una accion que se realiza despues de otra
 
 const {src, dest, watch, parallel} = require("gulp")
+//CSS
 const sass = require("gulp-sass")(require("sass"))
 const plumber = require("gulp-plumber") //para que no se detenga la ejecucion de gulp al escribir mal un bloque css
+const autoprefixer = require("autoprefixer")
+const cssnano = require("cssnano")
+const postcss = require("gulp-postcss")
+const sourcemaps = require("gulp-sourcemaps")
+//Imagenes
 const cache = require("gulp-cache")
 const imagemin = require("gulp-imagemin")
 const webp = require("gulp-webp")
 const avif = require("gulp-avif")
 
+//JS
+const terser = require("gulp-terser-js")
+
 //compilar los archivos de sass y pasarlo a css
 function css(done) {//se le puede poner done, cb, o callback
     src("src/scss/**/*.scss")    //Recorre todas las carpetas e identifica cada archivo de SASS
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass())   //Compila el/los archivo de SASS
+        .pipe(postcss([autoprefixer(), cssnano()])) //hago compatible el css con todos los navegadores y achico su peso
+        .pipe(sourcemaps.write("."))    //guarda el archivo llamado app.css.map en la misma ubicacion que elijo abajo
         .pipe(dest("build/css"))  //Almacenarlo en el archivo css
     done()  //se llama a este callback para indicarle a la funcion que acá termina su ejecucion. Sin esto nos sale msj de error de gulp
 }
@@ -54,6 +66,9 @@ function versionAvif(done) {
 //Los scripts que cree en src/js los envio al build para que estén en el deployment cuando suba la carpeta build
 function javascript(done) {
     src("src/js/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(terser()) //optimizo el codigo js
+        .pipe(sourcemaps.write("."))    //se guarda el archivo en app.js.map. Esto es para que se pueda acceder al codigo fuente  de los eventos. Por ej. en el navegador > source > interrupciones del objeto > mouse > click. Seleccionar el tilde, hacer click en alguna parte de la pagina que reaccione a ese click y se mostrará el codigo fuente de lo que hace
         .pipe(dest("build/js"))
     done()
 }
@@ -65,10 +80,10 @@ function dev(done) {
     done()
 }
 
-//exporto las funciones para hacerlas disponibles en package.json
 exports.css = css;
 exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.versionAvif = versionAvif
-exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);   //gulp parallel hace que se ejecuten a la vez estas 2 funciones
+exports.versionAvif = versionAvif;
+//exporto las funciones para hacerlas disponibles en package.json
+exports.dev = parallel(css, imagenes, versionWebp, versionAvif, javascript, dev);   //gulp parallel hace que se ejecuten a la vez estas 2 funciones
